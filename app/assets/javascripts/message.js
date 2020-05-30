c9eed057da54754276df44087ebe45cf76bbaa8b
issei-m-0206 <i.m-0206@docomo.ne.jp>
2020-05-30 11:14:23 +0900

    add_incremental_search

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -56,7 +56,6 @@ $(function(){
     })
     .done(function(data){
       var html = buildHTML(data);
-  
       $('.messages').append(html);
       $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight}); 
       $('form')[0].reset();
